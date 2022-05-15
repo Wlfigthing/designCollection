e6ac536a504dc2a63eaccb2cf46bc6e912200675
wl <2695070944@qq.com>
2022-05-15 21:38:52 +0800

    hot-fix 5 commit

--- src/main/java/com/example/designspringboot/Collection/ArrayLists.java
+++ src/main/java/com/example/designspringboot/Collection/ArrayLists.java
@@ -7,7 +7,7 @@ import java.util.*;
 public class ArrayLists {
     public static void main(String[] args) {
        TreeMap treeMap = new TreeMap();
-       treeMap.put("111",111);
+       treeMap.put("111",123);
        treeMap.put("111",111);
        treeMap.put("111",111);
        treeMap.put("111",111);
